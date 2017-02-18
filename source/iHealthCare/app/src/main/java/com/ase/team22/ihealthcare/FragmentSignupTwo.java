03d31be1542b4a76a54d9ffd7882fe6843c50157
kalyankilaru <005kvs@gmail.com>
2017-02-17 19:06:09 -0600

    Register Activity by kalyan

---
+++ source/iHealthCare/app/src/main/java/com/ase/team22/ihealthcare/FragmentSignupTwo.java
@@ -0,0 +1,23 @@
+package com.ase.team22.ihealthcare;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class FragmentSignupTwo extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup_two, container, false);
+    }
+
+}
