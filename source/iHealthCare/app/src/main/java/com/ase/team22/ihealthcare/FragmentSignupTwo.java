f79ec13a7d14e89743b266ce6180836862469c51
Chaitanyaperavali <chaitu.per@gmail.com>
2017-02-26 21:13:18 -0600

    signup activity

--- source/iHealthCare/app/src/main/java/com/ase/team22/ihealthcare/FragmentSignupTwo.java
+++ source/iHealthCare/app/src/main/java/com/ase/team22/ihealthcare/FragmentSignupTwo.java
@@ -1,13 +1,16 @@
 package com.ase.team22.ihealthcare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class FragmentSignupTwo extends Fragment {
@@ -17,7 +20,16 @@ public class FragmentSignupTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_two, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_signup_two, container, false);
+        Button btn = (Button) view.findViewById(R.id.button_Signup);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Home.class);
+                startActivity(intent);
 
+            }
+        });
+        return view;
+    }
 }
