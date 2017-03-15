fc692847d88a3fbce09867b2ee19f41f1b416a60
Chaitanyaperavali <chaitu.per@gmail.com>
2017-03-14 20:57:59 -0500

    m

--- source/iHealthCare/app/src/main/java/com/ase/team22/ihealthcare/questions/GroupSingle.java
+++ source/iHealthCare/app/src/main/java/com/ase/team22/ihealthcare/questions/GroupSingle.java
@@ -31,7 +31,8 @@ import java.util.ArrayList;
  * create an instance of this fragment.
  */
 public class GroupSingle extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+    // TODO: Rename parameter argument
+    // s, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "jsonResponse";
     public static final String tag =  "GroupSingle";
